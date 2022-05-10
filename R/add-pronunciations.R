92a23d8c1ec2391257dcc1c97cfe9b1a907952ce
TJ Mahr <tjmahr@users.noreply.github.com>
2022-05-10 14:22:38 -0500

    add islex

--- R/add-pronunciations.R
+++ R/add-pronunciations.R
@@ -32,6 +32,8 @@ subset_neighbors <- function(data, pronunciation_col, target_string, ...) {
 }
 
 
+
+
 df |>
   subset_neighbors(phones, "D OW N T") |>
   left_join(freq) |>
@@ -43,3 +45,46 @@ df |>
   left_join(freq) |>
   filter(!is.na(n), pronunciation_number == 1) |>
   print(n = Inf)
+
+
+islex <- read_lines("https://raw.githubusercontent.com/timmahrt/pysle/main/pysle/data/ISLEdict.txt")
+
+
+df2 <- islex |>
+  str_remove_all("[ˈˌ]") |>
+  str_remove_all(fixed(". ")) |>
+  # str_replace_all("#.*", "") |>
+  str_split_fixed(" # ", 2) |>
+  as.data.frame() |>
+  magrittr::set_colnames(c("word", "phones")) |>
+  as_tibble()
+
+
+
+df2 <- df2 |>
+  mutate(
+    note = stringr::str_extract_all(word, "[(].+[)]"),
+    word = word |>
+      stringr::str_remove_all("[(].+[)]") |>
+      str_replace_all("_", " "),
+    phones = phones |> str_remove_all(" #")
+  ) |>
+  unnest(note) |>
+  group_by(word) |>
+  mutate(pronunciation_number = seq_along(phones)) |>
+  ungroup()
+
+df2
+df2 |>
+  filter(word == "don't")
+
+df2 |>
+  subset_neighbors(phones, "d oʊ n t") |>
+  left_join(freq) |>
+  filter(!is.na(n), pronunciation_number == 1) |>
+  print(n = Inf)
+
+
+df2 |>
+  inner_join(freq) |>
+  write_csv("freqs-with-phones-ipa.csv")
